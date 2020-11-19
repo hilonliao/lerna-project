c2ad9523e3722884c6b204d6859bc5b2442ea344
hilonliao <hilonliao@tencent.com>
2020-11-19 19:52:40 +0800

    modify a module

--- packages/module-a/lib/module-a.js
+++ packages/module-a/lib/module-a.js
@@ -1,6 +1,6 @@
 const moduleB = require('module-b');
 const moduleA = function() {
-    console.log('hello world moduleA');
+    console.log('hello world moduleA~');
     console.log(moduleB());
 }
 module.exports = moduleA;
